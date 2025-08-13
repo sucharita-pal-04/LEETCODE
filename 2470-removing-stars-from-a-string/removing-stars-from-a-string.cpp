54e7991c9c77d4c36ebd49c81e09c0c59104ed99
sucharita-pal-04 <sucharita.pal04@gmail.com>
2025-08-13 10:38:14 +0530

    Time: 22 ms (38.76%) | Memory: 28.4 MB (63.63%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char> st;
+        string st; // acts like a stack
 
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '*') {
+        for (char c : s) {
+            if (c == '*') {
                 if (!st.empty()) {
-                    st.pop(); // remove last valid char
+                    st.pop_back(); // remove last char
                 }
-                s.erase(i, 1); // remove star from string
-                i--; // adjust index after erase
             } else {
-                st.push(s[i]);
+                st.push_back(c); // push into "stack"
             }
         }
-
-        // Build result from stack
-        string result;
-        while (!st.empty()) {
-            result.push_back(st.top());
-            st.pop();
-        }
-        reverse(result.begin(), result.end());
-
-        return result;
+        return st; // already the final string
     }
 };
